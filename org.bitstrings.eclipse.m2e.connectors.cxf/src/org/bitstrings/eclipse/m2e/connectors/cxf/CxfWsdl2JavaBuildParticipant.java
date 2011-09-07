34fe26c9999f1058ed18cf25f34277ee90c6f897
Spencer Uresk <suresk@gmail.com>
2011-09-07 15:33:45 -0600

    Changes to make the configurator work.

--- org.bitstrings.eclipse.m2e.connectors.cxf/src/org/bitstrings/eclipse/m2e/connectors/cxf/CxfWsdl2JavaBuildParticipant.java
+++ org.bitstrings.eclipse.m2e.connectors.cxf/src/org/bitstrings/eclipse/m2e/connectors/cxf/CxfWsdl2JavaBuildParticipant.java
@@ -11,20 +11,23 @@
 package org.bitstrings.eclipse.m2e.connectors.cxf;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.cxf.maven_plugin.WsdlOption;
+import org.apache.cxf.maven_plugin.WsdlOptionLoader;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecution;
+import org.bitstrings.eclipse.m2e.common.BuildHelper;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.IMaven;
 import org.eclipse.m2e.core.project.configurator.MojoExecutionBuildParticipant;
 import org.sonatype.plexus.build.incremental.BuildContext;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.cxf.maven_plugin.WsdlOption;
-import org.bitstrings.eclipse.m2e.common.BuildHelper;
 
 public class CxfWsdl2JavaBuildParticipant extends MojoExecutionBuildParticipant {
 
@@ -35,6 +38,7 @@ public class CxfWsdl2JavaBuildParticipant extends MojoExecutionBuildParticipant 
 	@Override
 	public Set<IProject> build(int kind, IProgressMonitor monitor)
 			throws Exception {
+		
 		final IMaven maven = MavenPlugin.getMaven();
 		final BuildContext buildContext = getBuildContext();
 		final MavenSession mavenSession = getSession();
@@ -49,19 +53,22 @@ public class CxfWsdl2JavaBuildParticipant extends MojoExecutionBuildParticipant 
 			filesModified = true;
 		} else {
 
-			final WsdlOption[] wsdlOptions = ((WsdlOption[]) maven
-					.getMojoParameterValue(mavenSession, mojoExecution,
-							"wsdlOptions", Object.class));
+			final List wsdlOptions = maven.getMojoParameterValue(mavenSession, mojoExecution, "wsdlOptions", List.class);
 
-			for (WsdlOption wsdlOption : wsdlOptions) {
-				String wsdl = wsdlOption.getWsdl();
+			// getMojoParameterValue returns an instance of WsdlOption from a different classloader, so casting doesn't work.
+			for (Object obj : wsdlOptions) {
+				Class k = obj.getClass();
+				Method getWsdl = k.getMethod("getWsdl");
+				Method getBindingFiles = k.getMethod("getBindingFiles");
+				
+				String wsdl = getWsdl.invoke(obj).toString();
 
 				filesModified = (!StringUtils.isEmpty(wsdl) && !ArrayUtils
 						.isEmpty(BuildHelper.getModifiedFiles(buildContext,
 								new File(wsdl))));
 
 				if (!filesModified) {
-					String[] bindingFiles = wsdlOption.getBindingFiles();
+					String[] bindingFiles = (String[])getBindingFiles.invoke(obj);
 
 					for (String bindingFile : bindingFiles) {
 						filesModified = (!StringUtils.isEmpty(bindingFile) && !ArrayUtils
